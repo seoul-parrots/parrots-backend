9b087eed7c192d5f32b5a3b1075c16bbf9fab134
FYLSunghwan <sunghwan@scatterlab.co.kr>
2022-08-21 10:40:37 +0900

    fix: cors 및 기타 버그 수정

--- parrots_backend/app.py
+++ parrots_backend/app.py
@@ -4,13 +4,22 @@ from uuid import uuid4
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from pydantic import BaseModel
 from starlette.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 FILE_PATH = "beaks"
 
 
 class FilePath(BaseModel):
-    path: str
+    index: str
 
 
 @app.on_event("startup")
@@ -21,18 +30,23 @@ def check_dir():
 
 @app.post("/upload")
 def upload(file: UploadFile = File()):
-    filename = os.path.join(FILE_PATH, str(uuid4()) + file.filename)
+    random_key = str(uuid4())
+    _, ext = os.path.splitext(file.filename)
+    filename = random_key + ext
+    fullpath = os.path.join(FILE_PATH, filename)
     bytes = file.file.read()
-    with open(filename, "wb") as f:
+    with open(fullpath, "wb") as f:
         f.write(bytes)
 
-    return FilePath(path=filename)
+    return FilePath(index=filename)
 
 
-@app.post("/download")
-def download(path: FilePath):
+@app.get("/download/{index}")
+def download(index: str):
 
-    if not os.path.exists(path.path):
+    if not os.path.exists(os.path.join(FILE_PATH, index)):
         raise HTTPException(status_code=404, detail="Item not found")
 
-    return FileResponse(path.path)
+    fullpath = os.path.join(FILE_PATH, index)
+
+    return FileResponse(fullpath)
